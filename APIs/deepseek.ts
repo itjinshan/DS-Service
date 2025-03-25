26be004f1f04891ffa67dd3f4c2a8e5ab8e4a819
Alex Jiang <it.jinshan@gmail.com>
2025-03-25 16:14:16 +0800

    feat: deepseek embeded

--- APIs/deepseek.ts
+++ APIs/deepseek.ts
@@ -1,15 +1,40 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
 var router = express.Router();
 var deepseek = require('../Deepseek/deepseek');
 
 router.get("/", (req: any, res: any) => {
-    res.send("Welcome to DS-Service deepseek API");
+    const authToken = req.body.token;
+    if(authToken) {
+        jwt.verify(authToken, process.env.DEEPSEEK_JWT_SECRET, (err: any, decoded: any) => {
+            if (err) {
+                res.status(401).send("Unauthorized");
+            } else {
+                res.send("Welcome to DS-Service deepseek API");
+            }
+        });
+    }
+    else {
+        res.status(401).send("Unauthorized");
+    }
 });
 
 router.get("/plantrip", (req: any, res: any) => {
-    deepseek(req.body.query).then((response: any) => {
-        res.send(response);
-    });
+    const authToken = req.body.token;
+    if(authToken) {
+        jwt.verify(authToken, process.env.DEEPSEEK_JWT_SECRET, (err: any, decoded: any) => {
+            if (err) {
+                res.status(401).send("Unauthorized");
+            } else {
+                deepseek(req.body.query).then((response: any) => {
+                    res.status(200).send(response);
+                });
+            }
+        });
+    }
+    else {
+        res.status(401).send("Unauthorized");
+    }
 });
 
 module.exports = router;
